bdc3dcef1e5f87966a5702c041104cd40c4356c1
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-28 14:45:22 +0800

    Updated server/client test.

--- ate-test.js
+++ ate-test.js
@@ -1,7 +1,18 @@
 
+const assert = require( "assert" );
 const ate = require( "./ate.js" );
 
 let test = { };
 ate( "sample", 123, test );
 
-console.log( Object.getOwnPropertyDescriptor( test, "sample" ) );
+let descriptor = Object.getOwnPropertyDescriptor( test, "sample" );
+
+assert.equal( descriptor.value, 123, "should return 123" );
+
+assert.equal( descriptor.writable, true, "should be true" );
+
+assert.equal( descriptor.enumerable, true, "should be true" );
+
+assert.equal( descriptor.configurable, false, "should be false" );
+
+console.log( "ok" );
